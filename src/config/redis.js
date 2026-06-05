ea89a5bb61bb4f2d809c13da5198872aefbde1a0
Piyush Verma <piy06072003ace@gmail.com>
2026-06-05 23:57:50 +0530

    redis 5 june update 2

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,8 +1,11 @@
 const redis = require('redis');
 
+
+
+
 const redisClient = redis.createClient({
     username: 'default',
-    password: 'process.env.REDIS_PASSWORD',
+    password: process.env.REDIS_PASSWORD,
     socket: {
         host: 'redis-16347.c11.us-east-1-3.ec2.cloud.redislabs.com',
         port: 16347
